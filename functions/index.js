b2f070d4e3afc8b0a093895882f2d9171949dd2f
Shopnamoy-Saha-Pronoy <sswapnamoy2016@gmail.com>
2023-07-23 15:33:34 +0600

    final

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,27 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-const express = require("express")
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+const functions = require('"firebase-functions');
+const express = require("express");
+const cors = require ('cors');
+const stripe = require('stripe')('sk_test_51NWYMNIc8VxG7WArL4j9d2bUhwSqkLSs26anTBpgTuSj11Z2b705kcHxz41w01H2lpVj4f80XsnBga2OT4PxTdfY00rSOGrWh5')
+//api
+
+//App config
+const app =express();
+
+//middlewares
+app.use(cors({origin:true}));
+app.use(express.json());
+
+
+//-api routes
+app.get('/',(request, response)=>response.status(200).send('Hellow World'))
+
+//-listen command
+exports.api = functions.onRequest(app)
+
+
+
